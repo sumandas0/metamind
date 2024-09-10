5383e338a054a7c07134e8e2a840872c8aac4a34
Suman Das <59254445+sumandas0@users.noreply.github.com>
2024-09-10 18:32:04 +0530

    fix: update postgres conn creation state

--- indexstore/__init__.py
+++ indexstore/__init__.py
@@ -1,4 +1,7 @@
 import configparser
+import os
+import psycopg2
+
 from indexstore.index_store import IndexStore
 from indexstore.postgres import PostgresIndexStore
 
@@ -8,6 +11,8 @@ def get_index_store() -> IndexStore:
     index_config.read("index.config")
     match index_config["Index"]["store"]:
         case "postgres":
-            return PostgresIndexStore()
+            conn_string = os.getenv("PG_CONN_STR")
+            conn = psycopg2.connect(conn_string)
+            return PostgresIndexStore(conn)
         case _:
             raise ValueError(f"Invalid index store type: {index_config['Index']['store']}")
